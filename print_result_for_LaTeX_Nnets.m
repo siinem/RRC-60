418b15a5b91f295ccb5afb73cdbda7a2a9857c8e
siinem <siinem@users.noreply.github.com>
2019-10-22 18:08:13 +0200

    Delete print_result_for_LaTeX_Nnets.m

--- print_result_for_LaTeX_Nnets.m
+++
@@ -1,9 +0,0 @@
-
-function print_result_for_LaTeX_Nnets(param,caption,accuracy)
-for n = 1:param.num_nets
-    tex = ['   ', char(caption),' N', num2str(n)];
-    for i=1:length(param.set)
-        tex = [tex ' & ' num2str(round(accuracy(n,i)*100,1))];
-    end
-    fprintf([tex(3:end) ' \\' '\\' '\n'])
-end

010cdb11782f96ce882ead2ec232dba6df11b7ff
siinem <siinem@users.noreply.github.com>
2019-10-22 18:08:07 +0200

    Delete print_result_for_LaTeX_1net.m

--- print_result_for_LaTeX_1net.m
+++
@@ -1,8 +0,0 @@
-
-function print_result_for_LaTeX_1net(param,caption,accuracy)
-
-tex = ['   ', char(caption)];
-for i=1:length(param.set)
-    tex = [tex ' & ' num2str(round(accuracy(i)*100,1)) ];
-end
-fprintf([tex(3:end) ' \\' '\\' '\n'])

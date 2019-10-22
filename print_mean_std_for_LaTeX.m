ba69eef162d91cb024b190ab92d84d0f6d3bf3bc
siinem <siinem@users.noreply.github.com>
2019-10-22 18:07:57 +0200

    Delete print_mean_std_for_LaTeX.m

--- print_mean_std_for_LaTeX.m
+++
@@ -1,8 +0,0 @@
-
-function print_mean_std_for_LaTeX(param,caption,mu_net,std_net)
-
-tex = ['   ', char(caption)];
-for i=1:length(param.set)
-    tex = [tex ' & ' num2str(round(mu_net(i)*100,1)) '$\\pm$' num2str(round(std_net(i)*100,1))];
-end
-fprintf([tex(3:end) ' \\' '\\' '\n'])
